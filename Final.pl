b61b50f8ef6ed615c20a6c71901120040071a69d
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-01-09 22:40:50 +0100

    Update Final.pl

--- Final.pl
+++ Final.pl
@@ -6,6 +6,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % DCG Parser
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Divide the height and width of the poster
-sizeofposter(Width, Height) --> [NumberxNumber], {re_split(x, NumberxNumber, [Width, _, Height])}.
+% Divide the height and width of the poster and make them integers
+to_integer(WidthxHeight, Width, Height):-
+    re_split(x, WidthxHeight, [W, _, H]),
+    atom_number(H, Height),
+    atom_number(W, Width).
+% The DCG Grammer for height and width
+sizeofposter(Width, Height) --> [WidthxHeight], {to_integer(WidthxHeight, Width, Height)}.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
