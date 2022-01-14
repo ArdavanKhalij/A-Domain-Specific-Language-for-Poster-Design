ec136ec330bee23bd8f725e042f675c18bc170d9
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-01-14 20:43:01 +0100

    Update Final.pl
    position property. All available positions

--- Final.pl
+++ Final.pl
@@ -13,9 +13,22 @@ to_integer(WidthxHeight, Width, Height):-
     atom_number(W, Width).
 % each property of the types
 % dimensions property
-dimensions_command(Width, Height) --> ['\t', dimensions, :, WidthxHeight], {to_integer(WidthxHeight, Width, Height)}.
+dimensions_command(Width, Height) --> ['\t', dimensions, :, WidthxHeight, '\n'], {to_integer(WidthxHeight, Width, Height)}.
 % filename property
-filename_command(FileName) --> ['\t', filename, :, '"',  FileName, '"'].
+filename_command(FileName) --> ['\t', filename, :, '"',  FileName, '"', '\n'].
 % content property
-content_command(Content) --> ['\t', content, :, '"'|Content], ['"'].
+content_command(Content) --> ['\t', content, :, '"'|Content], ['"', '\n'].
+% image property
+source_command(Source) --> ['\t', source, :, '"', Source, '"', '\n'].
+% position property
+% All available positions
+available_position(top-edge).
+available_position(bottom-edge).
+available_position(left-edge).
+available_position(right-edge).
+available_position(top-left).
+available_position(top-right).
+available_position(bottom-left).
+available_position(bottom-right).
+position_command(Position) --> ['\t', position, :, Position, '\n'], {available_position(Position)}.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
